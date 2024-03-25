f6eccdf6dc45c033a8234a5a6cd9311c38db0332
Edouard26 <edouard.halimi@gmail.com>
2024-03-25 13:51:40 +0100

    Create a header file and create a function for print character in terminal

---
+++ main.h
@@ -0,0 +1,10 @@
+#ifndef HEADER_H
+#define HEADER_H
+	
+	int _putchar(char c);
+	int _printf(const char *format, ...);
+        void print_char(char c);
+        void print_integer(int i);
+        void print_string(char c);
+        void print_perc(char c);
+#endif
