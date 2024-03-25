b854308b45342cd32243c42663f6e06a420ea14b
jydzip <dckjeremy.zip@gmail.com>
2024-03-25 13:28:11 +0100

    Addition structure of formats

--- main.h
+++ main.h
@@ -1,10 +1,23 @@
 #ifndef HEADER_H
 #define HEADER_H
-	
-	int _putchar(char c);
-	int _printf(const char *format, ...);
-        void _print_char(va_list args);
-        void _print_integer(va_list args);
-        void _print_string(va_list args);
-        void _print_perc(va_list args);
+#include <stdarg.h>
+
+/**
+ * struct format_types - Struct format
+ * @op: Character to recognize the format
+ * @f: Function to execute print
+ */
+typedef struct format_types
+{
+	char *op;
+	int (*f)(va_list args);
+} format_t;
+
+int _putchar(char c);
+int _printf(const char *format, ...);
+
+void _print_char(va_list args);
+void _print_integer(va_list args);
+void _print_string(va_list args);
+void _print_perc(va_list args);
 #endif
