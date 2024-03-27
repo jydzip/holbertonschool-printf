400f48b5ed88fe6de68b632407284c6b25feb14f
Edouard26 <edouard.halimi@gmail.com>
2024-03-27 11:28:06 +0100

    fix betty _printf.c

--- _printf.c
+++ _printf.c
@@ -1,28 +1,19 @@
 #include "main.h"
 #include <stdarg.h>
 #include <stddef.h>
-
+int (*get_format_function(char c2))(va_list);
 /**
  * _printf - ...
  * @format: String to print and to formated
  * Return: (int)
  */
+
 int _printf(const char *format, ...)
 {
 	int i = 0, ii, inc, count = 0;
 	char c1, c2;
 	va_list args;
 
-	format_t ftypes[] = {
-		{"c", _print_char},
-		{"s", _print_string},
-		{"i", _print_integer},
-		{"d", _print_integer},
-		{"%", _print_perc},
-		{"u", _print_unsigned_integer},
-		{NULL, NULL}
-	};
-	
 	va_start(args, format);
 	while (format[i] != '\0')
 	{
@@ -34,20 +25,17 @@ int _printf(const char *format, ...)
 		if (c1 == '%')
 		{
 			c2 = format[i + 1];
-
 			if (c2 == '\0')
 			{
 				va_end(args);
 				return (-1);
 			}
-
 			for (ii = 0; ii < 6; ii++)
 			{
-				if (ftypes[ii].op[0] == c2)
+				if (get_format_function (c2))
 				{
 					count += ftypes[ii].f(args);
 					inc = 2;
-
 					printed = 1;
 					break;
 				}
@@ -58,9 +46,35 @@ int _printf(const char *format, ...)
 			count += 1;
 			_putchar(c1);
 		}
-
 		i += inc;
 	}
 	va_end(args);
 	return (count);
+}
+
+/**
+ * gettypes - function to return the array of structures
+ * Return: specifiers
+ */
+int (*get_format_function(char c2))(va_list)
+{
+	int ii;
+
+	format_t ftypes[] = 
+	{
+		{"c", _print_char},
+		{"s", _print_string},
+		{"i", _print_integer},
+		{"d", _print_integer},
+		{"%", _print_perc},
+		{"u", _print_unsigned_integer},
+		{NULL, NULL}
+	};
+	for (ii = 0; ii < 6; ii++)
+	{
+		if (ftypes[ii].op[0] == c2)
+		{
+			return (ftypes[ii].f);
+		}
+	}
 }
